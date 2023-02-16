a946f4892bf128f74296af110971da451b4dbcbc
fbildirici <fatihbildirici.dev@gmail.com>
2023-02-17 01:21:48 +0300

    commit

---
+++ setting_up_py_environment.py
@@ -0,0 +1,26 @@
+print ("Hello World")
+
+##############################
+# sayılar numbers ve karakter dizileri strings
+##############################
+
+#integer
+print(9)
+
+#float
+9.2
+
+#veritipibulma
+
+type(9.2)
+
+################################
+#Atamalar ve Değişkenler (Assignment and Variables)
+################################
+
+b = "hello ai era"
+
+a= 9
+c= 17
+
+a*c
