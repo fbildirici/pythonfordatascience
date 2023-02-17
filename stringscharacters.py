7fbad1092290a7c1a756b00fa582dc553156df98
fbildirici <fatihbildirici.dev@gmail.com>
2023-02-17 14:55:55 +0300

    string ve string metodları incelemesi

---
+++ stringscharacters.py
@@ -0,0 +1,44 @@
+########################
+# Karakter Dizileri (Strings)
+########################
+
+print("Academics")
+print('Academics')
+
+"Academics"
+
+name="John"
+name
+
+#### Çok Satırlı Karakter Dizileri #####
+
+
+long_str= """Veri Yapıları: Hızlı Özet
+Sayılar (Numbers): int, float, complex
+Karakter Dizileri (Strings): str
+List, Dictionary, Tuple, Set
+Boolean (TRUE-FALSE): bool"""
+
+### Karakter Dizilerinin Elemanlarına Erişmek###
+
+name
+name[0] #pythonda karakterler sıfırla başlar, ilk harfe erişim için
+
+name[3]
+
+### Karakter Dizilerinde Slice İşlemi ###
+
+name [0:2]
+
+long_str[0:10]
+
+long_str
+
+"veri" in long_str
+
+"Veri" in long_str #Case Sensitivity
+
+
+##### String (Karakter Dizisi) Metodları ######
+###############################################
+
