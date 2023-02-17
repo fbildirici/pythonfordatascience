c8ed64daad972b86ee9a1933f83f853bf9547a20
fbildirici <fatihbildirici.dev@gmail.com>
2023-02-17 14:12:26 +0300

    sayilar bölümü yazıldı

---
+++ datastructures.py
@@ -0,0 +1,52 @@
+#Veri Yapıları
+################################
+# - Veri yapılarına giriş
+# - Sayılar, numbers: int, float, complex
+# - Karakter dizileri (string) str
+# - Boolean (True-False) bool
+# - Liste (list)
+# - Sözlük (Dictionary)
+# - Demet (Tuple)
+# - Set
+
+# Sayılar - integer
+
+x=46
+type(x)
+
+# sayılar - float
+
+y=171.22324
+type(y)
+
+# sayılar - complex
+z= 2j+1
+type(z)
+
+# Karakter Dizileri (String)
+
+a= "Welcome to DevOps Era"
+type (a)
+
+# Boolean
+True
+False
+type(True)
+5==4
+3==2
+14==14
+
+type(3==2)
+
+# List
+
+x=["computational social sciences", "mis", "ctis", "cs"]
+type (x)
+
+# Sözlük - Dictionary
+b = {"name": "Fatih", "Age": "30"}
+type(b)
+
+# Tuple- demet
+d = ("python", "java", "javascript")
+type (d)
---
+++ numbers.py
@@ -0,0 +1,24 @@
+
+
+a=5
+b=10.5
+
+a*13
+a/7
+a*b/4
+a**2 #karealma
+
+
+# Tipleri değiştirmek
+# örnek olarak float olan b ifadesi integer yapma
+int(b)
+float(a)
+
+int (a * b /3)
+
+c = a * b /3
+
+c
+
+int(c)
+
