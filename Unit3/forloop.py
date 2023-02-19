f0ca36c1279d59f5bae85382c5d84f601597fce6
fbildirici <fatihbildirici.dev@gmail.com>
2023-02-19 18:26:38 +0300

    for loops starts

---
+++ Unit3/forloop.py
@@ -0,0 +1,10 @@
+#######################
+# Döngüler (Loops #####
+#######################
+
+#### For Loop #########
+
+students = ["John", "Mark", "Venessa", "Mariam"]
+
+students[0] #students listesinden ilkini getirir
+
