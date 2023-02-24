c9e9b3737b47663101a5fb345b0b78265013abf1
fbildirici <fatihbildirici.dev@gmail.com>
2023-02-24 21:00:55 +0300

    listeleri zip halinde gruplama gözden geçirildi

---
+++ Unit3/zip.py
@@ -0,0 +1,11 @@
+#############################################
+# zip
+#############################################
+
+students = ["Yuval", "Noah", "Harari", "Ian", "Goodfellow"]
+
+departments = ["MIS", "DataScience", "Mathematics", "Computer Science", "AI"]
+
+ages = ["23", "35", "42", "36", "48"]
+
+list(zip(students, departments, ages))
