a6ee262c19b38de97a8f62cd5a53bb2719e8e0bb
fbildirici <fatihbildirici.dev@gmail.com>
2023-02-26 14:59:51 +0300

    functions

---
+++ Exercises/exercise1typesofdescription.py
@@ -0,0 +1,47 @@
+#### veri tiplerini incele#####
+
+x = 8
+### integer çıkacak
+type(x)
+
+y = 3.2
+
+type (y) ##float
+
+z = 3j + 18
+
+type (z) ## complex
+
+a = "Data Science"
+
+type(a) ##string
+
+b = True
+
+type (b) ##boolean
+
+c = 23 < 22
+
+type (c) ##boolean
+
+c ##false çıktı
+
+l = [1, 2, 3, 4]
+
+type (l) #list
+
+t = {"Machine Learning", "Data Science"}
+
+type (t) ## tulip
+
+g = {"Name": "Jake",
+     "Age": 27,
+     "Address": "Nyu"}
+
+type(g) ##dictionary
+
+s = {"Python", "Machine Learning", "Data Science"}
+
+type(s) #set
+
+
